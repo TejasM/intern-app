72c0406bd83b6bfde1b0b26997eba2c6a368c378
Tejas <tmehta@redhat.com>
2012-05-15 16:54:05 -0400

    Not working due to email

--- src/main/java/org/jboss/tools/examples/model/Member.java
+++ src/main/java/org/jboss/tools/examples/model/Member.java
@@ -2,55 +2,47 @@ package org.jboss.tools.examples.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
 @Entity
 @XmlRootElement
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
    /** Default value included to remove warning. Remove or modify at will. **/
    private static final long serialVersionUID = 1L;
 
-   @Id
+  /* @Id
    @GeneratedValue
-   private Long id;
-
+   private Long id;*/
+   @Id
    @NotNull
    @Size(min = 1, max = 25)
    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
    private String name;
 
-   @NotNull
+   /*@NotNull
    @NotEmpty
    @Email
-   private String email;
+   private String email;*/
 
-   @NotNull
+   /*@NotNull
    @Size(min = 10, max = 12)
    @Digits(fraction = 0, integer = 12)
    @Column(name = "phone_number")
-   private String phoneNumber;
+   private String phoneNumber;*/
 
-   public Long getId() {
+   /*public Long getId() {
       return id;
    }
 
    public void setId(Long id) {
       this.id = id;
-   }
+   }*/
 
    public String getName() {
       return name;
@@ -60,7 +52,7 @@ public class Member implements Serializable {
       this.name = name;
    }
 
-   public String getEmail() {
+  /* public String getEmail() {
       return email;
    }
 
@@ -74,5 +66,5 @@ public class Member implements Serializable {
 
    public void setPhoneNumber(String phoneNumber) {
       this.phoneNumber = phoneNumber;
-   }
+   }*/
 }
--- src/test/java/org/jboss/tools/examples/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/tools/examples/test/MemberRegistrationTest.java
@@ -39,11 +39,11 @@ public class MemberRegistrationTest {
    public void testRegister() throws Exception {
       Member newMember = memberRegistration.getNewMember();
       newMember.setName("Jane Doe");
-      newMember.setEmail("jane@mailinator.com");
-      newMember.setPhoneNumber("2125551234");
+     /* newMember.setEmail("jane@mailinator.com");
+      newMember.setPhoneNumber("2125551234");*/
       memberRegistration.register();
-      assertNotNull(newMember.getId());
-      log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+      //assertNotNull(newMember.getId());
+      //log.info(newMember.getName() + " was persisted with id " + newMember.getId());
    }
    
 }
