934460945a64bbd002ba0b20c3b75466b9c1dac0
Tejas <tmehta@redhat.com>
2012-05-16 14:48:53 -0400

    Added the max limit feature - works like a charm but question about the lack of refresh (possibly need to rerender

--- src/main/java/org/jboss/tools/examples/data/ProjectListProducer.java
+++ src/main/java/org/jboss/tools/examples/data/ProjectListProducer.java
@@ -15,16 +15,22 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.jboss.tools.examples.model.Assignment;
 import org.jboss.tools.examples.model.Project;
 
 @RequestScoped
 public class ProjectListProducer {
 	@Inject
 	private EntityManager emProject;
+	
+	@Inject
+	private EntityManager emAssignment;
 
 	private List<Project> projects;
 
 	private List<String> projectsNames;
+
+	private List<Assignment> assignments2;
 
 	// @Named provides access the return value via the EL variable name
 	// "Projects" in the UI (e.g.,
@@ -34,6 +40,7 @@ public class ProjectListProducer {
 	public List<Project> getProjects() {
 		return projects;
 	}
+
 	@Produces
 	@Named
 	public List<String> getProjectsNames() {
@@ -61,15 +68,25 @@ public class ProjectListProducer {
 
 	private void toNames() {
 		// TODO Auto-generated method stub
-		if(projectsNames == null){
+		if (projectsNames == null) {
 			projectsNames = new ArrayList<String>();
 		}
+		projectsNames.clear();
 		// TODO Auto-generated method stub
 		for (int i = 0; i < projects.size(); i++) {
-			projectsNames.add(projects.get(i).getName());			
+			CriteriaBuilder cb = emAssignment.getCriteriaBuilder();
+			CriteriaQuery<Assignment> criteria = cb
+					.createQuery(Assignment.class);
+			Root<Assignment> Assignment = criteria.from(Assignment.class);
+			// Swap criteria statements if you would like to try out type-safe
+			// criteria queries, a new
+			// feature in JPA 2.0
+			// criteria.select(Assignment).orderBy(cb.asc(Assignment.get(Assignment_.name)));
+			criteria.select(Assignment).equals(projects.get(i));
+			assignments2 = emAssignment.createQuery(criteria).getResultList();
+			if(assignments2.size() < projects.get(i).getMaxPeople())
+				projectsNames.add(projects.get(i).getName());
 		}
 	}
-
-	
 
 }
